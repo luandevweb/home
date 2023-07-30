bf1ab93caa1a8a9737b44262f86cd027eed689b7
Luan DevWeb <luandevweb@gmail.com>
2023-07-30 19:53:13 -0300

    Alterações necessárias na home

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+var bizus = document.getElementsByClassName("bizus_links")
+
+var teste = Array.from(bizus)
+
+teste.forEach(element => {
+   element.addEventListener("click",()=>{
+
+alert("ATENÇÃO! Os sites abaixo foram desenvolvidos por min com o conceito apenas PEDAGÓGICO, sem nenhuma intenção de práticas de má fé, disponíveis apenas como PORTFÓLIO. ")
+
+   })
+});
